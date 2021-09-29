92fac87357c222db5dbece6122ba8d2da0cdb74a
Rahul Ahire <ahirerahul21@gmail.com>
2021-09-29 16:12:53 +0530

    working

--- handler.js
+++ handler.js
@@ -3,12 +3,15 @@ const cookie = require("cookie");
 
 module.exports.hello = async (event) => {
   const body = JSON.parse(event.body);
+  const name = body.name,
+  const value = body.value 
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://lambda-cookie.netlify.app",
       "Access-Control-Allow-Credentials": true,
-      "Set-Cookie": cookie.serialize('id', 'abcd-', {
+      "Set-Cookie": cookie.serialize(name, value, {
+        maxAge: 10
         
       }),
     },
