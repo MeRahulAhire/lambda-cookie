baa2d2b5e4d6cb650d1cf4e6dca48305406189ae
Rahul Ahire <ahirerahul21@gmail.com>
2021-09-29 18:25:53 +0530

    adding route

---
+++ get.js
@@ -0,0 +1,28 @@
+"use strict";
+// const cookie = require("cookie");
+
+module.exports = async (event) => {
+  const body = JSON.parse(event.body);
+//   const name = body.name;
+//   const value = body.value;
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "https://lambda-cookie.netlify.app",
+      "Access-Control-Allow-Credentials": true,
+    //   "Set-Cookie": cookie.serialize(name, value, {
+    //     maxAge: 10,
+    //   }),
+    },
+    body: JSON.stringify(
+      {
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
